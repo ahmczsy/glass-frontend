e9c3bc0e1bcfa788cfa93b1ca691d3b596e23097
siyu.zhang <ahmczsy@gmail.com>
2019-07-20 23:41:17 +0800

    s

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -43,7 +43,6 @@
 
       <el-button :loading="loading" type="primary" style="width:100%;margin-bottom:30px;" @click.native.prevent="handleLogin">登陆</el-button>
 
-
     </el-form>
   </div>
 </template>
